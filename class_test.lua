4c11927f385f5861ab63d12972d9bffe7a37a7a9
fergoth <fergothew@gmail.com>
2019-09-14 20:47:53 +0300

    refactor test

--- class_test.lua
+++ class_test.lua
@@ -15,6 +15,7 @@ settings = {
   size_x = 25,
   size_y = 25
 }
+math.randomseed(os.time())
 local map =  require "generate_map"
 map1 = map:new()
 map2 = map:new()
@@ -22,3 +23,7 @@ map1:generate(settings)
 map2:generate(settings)
 map1:print()
 map2:print()
+submap1 = map1:return_submap(2,2,5,5)
+submap2 = map2:return_submap(2,2,5,5)
+submap1:print()
+submap2:print()
